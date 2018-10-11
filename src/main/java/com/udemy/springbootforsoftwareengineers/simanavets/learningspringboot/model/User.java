803bf9eeefe145cad6a5312e2abca8b9c1be950f
Mikita Simanavets <sema.nikita@gmail.com>
2018-10-11 17:50:51 +0200

    Add Java Bean Validation

--- src/main/java/com/udemy/springbootforsoftwareengineers/simanavets/learningspringboot/model/User.java
+++ src/main/java/com/udemy/springbootforsoftwareengineers/simanavets/learningspringboot/model/User.java
@@ -3,6 +3,10 @@ package com.udemy.springbootforsoftwareengineers.simanavets.learningspringboot.m
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -10,10 +14,23 @@ import java.util.UUID;
 public class User {
 
     private final UUID userUid;
+
+    @NotNull
     private final String firstName;
+
+    @NotNull
     private final String lastName;
+
+    @NotNull
     private final Gender gender;
+
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 112)
     private final Integer age;
+
+    @NotNull
+    @Email
     private final String email;
 
     public User(
